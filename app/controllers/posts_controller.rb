35288060f998abdfab3d311cf03250fb2e8e11f7
Raysa Aurrecochea <aurrecochearaysa@gmail.com>
2018-08-14 22:10:49 -0500

    add create delete and edit features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :build_post, only: [:new, :create]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index
     @title = "My Blog Dashboard"
@@ -6,18 +8,46 @@ class PostsController < ApplicationController
   end
 
   def new
-    @name = "Raysa"
+  end
+
+  def create
+    @post.title   = params[:post][:title]
+    @post.content = params[:post][:content]
+    # content it might be required!
+    if @post.save
+      redirect_to root_path
+    else
+      redirect_to new_post_path
+    end
   end
 
   def edit
   end
 
-  def create
-  end
-
   def update
+    # convevtion over configuration
+    @post.title   = params[:post][:title]
+    @post.content = params[:post][:content]
+    # content it might be required!
+    if @post.save
+      redirect_to root_path
+    else
+      redirect_to new_post_path
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path
   end
+
+  private
+
+    def build_post
+      @post = Post.new
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # resources generate the 7 refstul routes.
-  resources :posts
+  root "posts#index"
+  resources :posts, except:  [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
